1c49d5f5a199cdccb9e43600f858fbdf1ed97bfe
Pedro Reis <pedroreisv@gmail.com>
2017-05-16 17:30:19 -0300

    Add Joi validation userSchema

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi')
+
+const userSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required(),
+  confirmationPassword: Joi.any().valid(Joi.ref('password')).required()
+})
 
 router.route('/register')
   .get((req, res) => {
     res.render('register');
-  });
+  })
+  .post((req, res, next) => {
+    const result = Joi.validate(req.body, userSchema)
+
+  })
 
 router.route('/login')
   .get((req, res) => {
